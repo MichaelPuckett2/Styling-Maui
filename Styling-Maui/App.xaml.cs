7594bd753c513cf966d707b1564b166f90aed410
Michael Pucket <michael.puckett.ii@outlook.com>
2023-12-14 16:02:38 -0500

    Init commit

---
+++ Styling-Maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Styling_Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Styling-Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Styling_Maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
